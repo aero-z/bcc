a6ad4da65a733c81629b82a12bd2389c0667a0c6
aero-z <aero_super@yahoo.com>
2013-01-24 18:11:30 -0500

    Added some comments to Token classes

--- src/scanner/Token.scala
+++ src/scanner/Token.scala
@@ -4,18 +4,30 @@ class Token(str: String) {
 
 }
 
+/**
+ * Java keyword tokens
+ */
 case class KeywordToken(str: String) extends Token(str: String) {
   
 }
 
+/**
+ * Identifiers like fields and method names
+ */
 case class IdentifierToken(str: String) extends Token(str: String) {
   
 }
 
+/**
+ * Tokens for scoping purposes, like { and (
+ */
 case class ScopingToken(str: String) extends Token(str: String) {
   
 }
 
+/**
+ * Represents literals like string and number constants
+ */
 case class LiteralToken(str: String) extends Token(str: String) {
   
 }
