0288f934717b29a17007557a19f8901234771813
aero-z <aero_super@yahoo.com>
2013-02-14 14:01:52 -0500

    Added simple main test

--- src/main/Joosc.scala
+++ src/main/Joosc.scala
@@ -7,6 +7,21 @@ import parser.Dfa
 import parser.ParseException
 
 object Joosc {
+  
+  val errCodeSuccess = 0
+  val errCodeParseErr = 42
+  
+  def compile(source: Source): Int = {
+    val dfa = Dfa.fromFile(Source.fromFile("cfg/Joos1W.lr1"))
+    try {
+    	val parseTree = Parser.parse(Scanner.scan(source.mkString), dfa)
+    } catch {
+      case e: ParseException => 
+        Console.err.println("Parse error")
+        return errCodeParseErr
+    }
+    errCodeSuccess
+  }
 
   def main(args: Array[String]): Unit = {
     if (args.length != 1) {
@@ -14,14 +29,8 @@ object Joosc {
       System.exit(1)
     }
     val file = Source.fromFile(args(0))
-    val dfa = Dfa.fromFile(Source.fromFile("cfg/Joos1W.lr1"))
-    try {
-    	val parseTree = Parser.parse(Scanner.scan(file.mkString), dfa)
-    } catch {
-      case e: ParseException => 
-        Console.err.println("Parse error")
-        System.exit(42)
-    }
+    val ret = compile(file)
+    System.exit(ret)
   }
 
 }
