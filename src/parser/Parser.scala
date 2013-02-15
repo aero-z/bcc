70ab2634990b8c5ed4e1a70c204ee2a9a147bdf4
bastienjacot-guillarmod <bjacotg@gmail.com>
2013-02-14 20:47:49 -0500

    Going crazy

--- src/parser/Parser.scala
+++ src/parser/Parser.scala
@@ -14,7 +14,7 @@ object Parser {
 	            	input.head match {
 	            	  case EndToken() => assert(stack.length == 2); stack.head._1
 	            	  case x =>	
-	            	    println("shift: "+x) //TESTING
+	            	    println(s"shift: $x") //TESTING
 	            	    parseRec((input.head, state) :: stack, input.tail)
 	            	}
 	            case ReduceAction(rule) =>
@@ -22,7 +22,7 @@ object Parser {
 	                val (stackRemain, newSymbol) = reduce(stack, rule);
 	                parseRec(stackRemain, newSymbol :: input)
 	            case ErrorAction() =>
-	                throw new ParseException("invalid input")
+	                throw new ParseException(s"invalid input, state: ${stack.head._2} input: ${input.head}")
             }
         }
         def reduce(stack: List[(Symbol, Dfa.State)], rule: Rule): (List[(Symbol, Dfa.State)], Symbol) = {
--- test/parser/Newtest.scala
+++ test/parser/Newtest.scala
@@ -5,17 +5,36 @@ import org.scalatest.FunSuite
 import scanner._
 import parser._
 
-class Newtest extends FunSuite{
-  val myCode = """class myClass {
+class Newtest extends FunSuite {
+    val codeList = List("""class myClass {}""",
+            """package edu.syr.pcpratts.jpp.templates;
+import edu.syr.pcpratts.jpp.cfile.CFileItem;
+import edu.syr.pcpratts.jpp.cfile.CFileVisitor;
+class MethodDeclFinderPass extends CFileVisitor {
+  public MethodDeclFinderPass() {
+    int rouge = 32;
+            rouge.salope = rouge;
+            }
+  protected void visitItem(CFileItem item) {
+  }
+}""")
+    val myCode = """class myClass {
 }"""
-  test("Everything") {
-    val tokens = Scanner.scan(myCode)
-	tokens.foreach(x => println(x.getClass()))
-	println("SCANNING DONE")
-	val dfa = Dfa.fromFile(Source.fromFile(new java.io.File("cfg/eclipse.lr1")))
-	println("BUILDING DFA DONE")
-	val parseTree = Parser.parse(tokens, dfa)
-	println("BUILDING PARSETREE DONE")
-	Parser.printTree(parseTree)
-  } 
+    test("Everything") {
+        codeList foreach testCode
+    }
+
+    def testCode(code: String) {
+        val tokens = Scanner.scan(code)
+        tokens.foreach(x => println(s"${x.getClass()} ${x.typeStr}"))
+        println("SCANNING DONE")
+        val dfa = Dfa.fromFile(Source.fromFile(new java.io.File("cfg/eclipse.lr1")))
+        println("BUILDING DFA DONE")
+        val parseTree = Parser.parse(tokens, dfa)
+        println("BUILDING PARSETREE DONE")
+        Parser.printTree(parseTree)
+        println("==========================")
+        println("TEST DONE")
+        println("==========================")
+    }
 }
