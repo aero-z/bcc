a0f67555ae90ec9b254839610cb19f96cdf0f268
CedricBastin <cedricbastin@gmail.com>
2013-02-14 22:07:22 -0500

    AST update

--- src/parser/AST.scala
+++ src/parser/AST.scala
@@ -31,33 +31,56 @@ case class EmptyNode() extends Node {
 }
 case class EndNode(info:String) extends Node
 case class BlockNode(list:List[Node]) extends Node
-case class Assignment(leftNode:Node, rightNode:Node) extends Node
+case class AssignmentNode(leftNode:Node, rightNode:Node) extends Node
 case class IfNode(condition:Node, thenblock:Node, elseBlock:Node) extends Node {
   def toString = "if ( "+condition.toString+" ) "+thenblock.toString()+" "+elseBlock.toString()
 }
 case class WhileNode(condition:Node, block:Node) extends Node {
   def toString() = "while ( "+condition.toString+" ) "+block.toString 
 }
+case class OperationNode(leftSide:Node, operator:String, rightSide:Node) extends Node
 
 object AST {
+  
+  def buildAST(symbol:Symbol):Node = {
+    reduceSimpleBranches(toNode(Symbol))
+  }
+  
 	val ifstatement = "(IfThenElseStatement|IfThenElseStatementNoShortIf)".r //the same after parsing
 	val whilestatement = "(WhileStatement|WhileStatementNoShortIf)".r
+	val operations = "(MultiplicativeExpression|AdditiveExpression|RelationalExpression|EqualityExpression|CondOrExpression|CondAndExpression)".r
 	def toNode(symbol:Symbol):Node = symbol match {
 	  case NonTerminalSymbol("IfThenStatement", list) => IfNode(toNode(list(3)), toNode(list(5)), EmptyNode())
 	  case NonTerminalSymbol(ifstatement(_), list) => IfNode(toNode(list(3)), toNode(list(5)), toNode(list(7)))
 	  case NonTerminalSymbol(whilestatement(_), list) => WhileNode(toNode(list(3)), toNode(list(5)))
-	  case NonTerminalSymbol("Assignment", list) => Assignment
-	  case NonTerminalSymbol("Happy", list) => Assignment
-	  
+	  case NonTerminalSymbol("Assignment", list) => AssignmentNode(toNode(list(1)), toNode(list(3)))
+	  case NonTerminalSymbol(operations(_), List(_,_,_)) => OperationNode(toNode(list(1)), list(2), toNode(list(3)))
 	  case NonTerminalSymbol(name, list) => BlockNode(list.map(toNode(_)))
 	  case x => EndNode(x.toString())
 	}
     
-    def reducdeSimpleBranches(symbol:Symbol):Symbol = symbol match {
+	def reducdeSimpleBranches(node:Node):Node = node match {
+	  case BlockNode(x::Nil) => reduceSimpleBranches(x)
+	  case BlockNode(x) => BlockNodes(x.map(reduceSimpleBranches(_)))
+	}
+	
+    def reducdeSimpleSymbolBranches(symbol:Symbol):Symbol = symbol match {
       case NonTerminalSymbol(name, list) =>
-        val newlist = list.map(reducdeSimpleBranches(_))
+        val newlist = list.map(reducdeSimpleSymbolBranches(_))
         if (newlist.length == 1) newlist.head
-        else NonTerminalSymbol(name, newlist)
+        else NonTerminalSymbol(name, List(_, _, _))
       case x => x
+    }
+    
+    def printTree(node:Node) {
+      def printRec(delim:String, tree:List[Symbol]) {
+        tree.foreach(
+          _ match {
+          	case NonTerminalSymbol(name, list) => println(delim+name); printRec(delim+"    ", list)
+          	case x => println(delim+x)
+          }   
+        )
+      }
+      printRec("", List(symbol))
     }
 }
