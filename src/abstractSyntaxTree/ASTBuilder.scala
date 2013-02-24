a313779eca244cc5507682ff7eedbb93d26a9a73
bastienjacot-guillarmod <bjacotg@gmail.com>
2013-02-24 17:58:01 -0500

    Still building the ast

--- src/abstractSyntaxTree/ASTBuilder.scala
+++ src/abstractSyntaxTree/ASTBuilder.scala
@@ -2,34 +2,98 @@ package abstractSyntaxTree
 
 import parser._
 import scanner._
+import abstractSyntaxTree.Modifier._
 
 object ASTBuilder {
+  // Start the extraction of the parse tree.
+  def build(parseTree: Symbol, fileName: String): CompilationUnit = parseTree match {
+    case NonTerminalSymbol("CompilationUnit", xs) => CompilationUnit(extractPackage(xs),
+      extractImport(xs), extractTypeDefinition(xs), fileName)
+    case _ => throw new ASTBuildingException("That is not a compilation unit")
+  }
 
-    def build(parseTree: Symbol, fileName: String): CompilationUnit = parseTree match {
-        case NonTerminalSymbol("CompilationUnit", xs) => CompilationUnit(extractPackage(xs),
-           extractImport(xs), extractTypeDefinition(xs), fileName)
-        case _ => throw new ASTBuildingException("That is not a compilation unit")
+  def extractPackage(symbols: List[Symbol]): Option[Name] = symbols.collectFirst({
+    case NonTerminalSymbol("PackageDeclaration", xs) => extractName(xs(1))
+  })
+
+  def extractImport(symbols: List[Symbol]): List[ImportDeclaration] = symbols.collect({
+    case NonTerminalSymbol("ImportDeclaration", List(_, name, _)) => ClassImport(extractName(name))
+    case NonTerminalSymbol("ImportDeclaration", List(_, name, _, _, _)) => PackageImport(extractName(name))
+  })
+  
+  def extractTypeDefinition(symbols: List[Symbol]): Option[TypeDefinition] = symbols.collectFirst({
+    case NonTerminalSymbol("TypeDeclaration", List(NonTerminalSymbol("ClassDeclaration", xs))) => val (fields,constructors, methods) = extractClassBody(xs)
+      ClassDefinition(extractIdentifier(xs), extractParent(xs), extractInterfaces(xs), extractModifiers(xs), fields, constructors, methods)
+    case NonTerminalSymbol("TypeDeclaration", List(NonTerminalSymbol("InterfaceDeclaration", xs))) => InterfaceDefinition(extractIdentifier(xs), extractInterfaces(xs), extractModifiers(xs), extractInterfaceMethods(xs))
+  })
+  
+  
+  def extractName(name: Symbol): Name = {
+    def recExtractName(name: Symbol, acc: List[String]): List[String] = name match {
+      case NonTerminalSymbol("Name", List(NonTerminalSymbol("QualifiedName", List(newName, _, simpleName)))) => recExtractName(newName, extractSimpleName(simpleName) :: acc)
+      case NonTerminalSymbol("Name", List(NonTerminalSymbol("SimpleName", List(simpleName)))) => extractSimpleName(simpleName) :: acc
+    }
+    def extractSimpleName(simpleName: Symbol): String = simpleName match {
+      case NonTerminalSymbol("SimpleName", List(IdentifierToken(id))) => id
     }
 
-    def extractPackage(symbols: List[Symbol]): Option[Name] = symbols.collectFirst({
-        case NonTerminalSymbol("PackageDeclaration", xs) => extractName(xs(1))
-    })
+    Name(recExtractName(name, Nil).reverse)
+  }
 
-    def extractImport(symbols: List[Symbol]): List[ImportDeclaration] = ???
-    def extractTypeDefinition(symbols: List[Symbol]): Option[TypeDefinition] = ???
-    def extractName(name: Symbol): Name ={
-        def recExtractName(name: Symbol): List[String] = name match {
-        case NonTerminalSymbol("Name", List(NonTerminalSymbol("QualifiedName", List(newName, ScopingToken("."), simpleName)))) => extractSimpleName(simpleName) :: recExtractName(newName)
-        case NonTerminalSymbol("Name", List(NonTerminalSymbol("SimpleName", List(simpleName)))) => List(extractSimpleName(simpleName))
+  
+  //Find the first identifier of a list of symbols, do not if there is multiple symbol
+  def extractIdentifier(symbols: List[Symbol]): String = symbols.collectFirst({
+    case IdentifierToken(id) => id
+  }).get
+  //find John Connor parent and save it from the Terminator
+  def extractParent(symbols: List[Symbol]): Option[RefType] = symbols.collectFirst({
+    case NonTerminalSymbol("ClassOrInterfaceType", List(name)) => RefType(extractName(name))
+  })
+  def extractInterfaces(symbols: List[Symbol]): List[RefType] = {
+    def recExtractInterfaces(symbols: List[Symbol], acc: List[RefType]): List[RefType] = symbols match{
+      case List(NonTerminalSymbol("Interfaces",newinterfaces), _, interface) => recExtractInterfaces(newinterfaces, extractInterface(interface)::acc)
+      case List(NonTerminalSymbol("Interface", List(interface))) => extractInterface(interface) :: acc
     }
-        def extractSimpleName(simpleName: Symbol): String = simpleName match{
-            case NonTerminalSymbol("SimpleName", List(IdentifierToken(id))) => id
-        }
-
-        Name(recExtractName(name).reverse)
+    def extractInterface(interface: Symbol): RefType = interface match{
+      case NonTerminalSymbol("Interface", List(NonTerminalSymbol("ClassOrInterfaceType", List(name)))) => RefType(extractName(name))
     }
     
+    symbols.collectFirst({case NonTerminalSymbol("Interfaces", xs) => xs}) match{
+      case Some(x) => recExtractInterfaces(x, Nil)
+      case None => Nil
+    }
+  }
+  def extractModifiers(symbols: List[Symbol]): List[Modifier] = {
+    def recExtractModifiers(symbols: List[Symbol], acc: List[Modifier]): List[Modifier] = symbols match{
+      case List(NonTerminalSymbol("Modifiers",newmodifier), modifier) => recExtractModifiers(newmodifier, extractModifier(modifier)::acc)
+      case List(modifier) => extractModifier(modifier) :: acc
+    }
+    def extractModifier(modifier: Symbol): Modifier = modifier match{
+      case NonTerminalSymbol("Modifier", List(KeywordToken(str))) => Modifier.fromString(str)
+    }
+    
+    symbols.collectFirst({case NonTerminalSymbol("Modifiers", xs) => xs}) match{
+      case Some(x) => recExtractModifiers(x, Nil)
+      case None => Nil
+    }
+  }
+  
+  def extractClassBody(symbols: List[Symbol]): (List[FieldDeclaration], List[ConstructorDeclaration], List[MethodDeclaration]) ={
+    def recExtractClassBody(symbols: List[Symbol], fieldAcc: List[FieldDeclaration], constructorAcc: List[ConstructorDeclaration], methodAcc: List[MethodDeclaration]): (List[FieldDeclaration], List[ConstructorDeclaration], List[MethodDeclaration]) = symbols match{
+      case List(NonTerminalSymbol("ClassBodyDeclarations", nextDeclarations), NonTerminalSymbol("ClassBodyDeclaration", bodyDeclaration)) => ???//TODO do something with that
+    case List(NonTerminalSymbol("ClassBodyDeclaration", bodyDeclaration)) => ??? //TODO other stuff
+  }
 
-    class ASTBuildingException(msg:String) extends Exception(msg)
+
+  symbols.collectFirst({case NonTerminalSymbol("ClassBodyDeclarations", xs) => xs}) match{
+    case Some(x) => recExtractClassBody(x, Nil, Nil, Nil)
+    case None => (Nil, Nil, Nil)
+  }
+
+}
+def extractInterfaceMethods(symbols: List[Symbol]): List[MethodDeclaration] = Nil//TODO implement
+
+
+class ASTBuildingException(msg: String) extends Exception(msg)
 
 }
--- src/abstractSyntaxTree/CompilationUnit.scala
+++ src/abstractSyntaxTree/CompilationUnit.scala
@@ -6,36 +6,96 @@ import scanner.IntegerToken
 import scala.Enumeration
 
 //Will be used quite often, is for instance "java.util.String"
-case class Name(path: List[String]) extends Expression{
-    override def toString = path.reduce((x, y)=> x + "." + y)
+case class Name(path: List[String]) extends Expression {
+    override def toString = path.reduce((x, y) => x + "." + y)
 }
 
 //Main  of a file.
-case class CompilationUnit(packageName: Option[Name], classImport: List[ImportDeclaration]
-        , typeDef: Option[TypeDefinition], fileName: String)
+case class CompilationUnit(packageName: Option[Name], importDeclarations: List[ImportDeclaration], typeDef: Option[TypeDefinition], fileName: String) {
+    def display(): Unit = {
+        println("*" * 20)
+        println("Compilation Unit")
+        println("*" * 20)
+        println(s"File Name: $fileName")
+        println(s"Package defined: ${packageName.isDefined}")
+        for (x <- packageName) println(s"Package name: ${packageName.toString}")
+        println(s"Number of import: ${importDeclarations.size}")
+        for (ClassImport(name) <- importDeclarations) println(s"Import class: ${name.toString}")
+        for (PackageImport(name) <- importDeclarations) println(s"Import package: ${name.toString}")
+        println("*" * 20)
+        println()
+        for (typeDefinition <- typeDef) typeDefinition.display
+    }
+}
 
 abstract class ImportDeclaration(name: Name)
 
 case class ClassImport(name: Name) extends ImportDeclaration(name)
 case class PackageImport(name: Name) extends ImportDeclaration(name)
-        
-        
+
 //Either a class or an interface
-abstract class TypeDefinition(typeName: String)
+abstract class TypeDefinition(typeName: String) {
+    def display: Unit
+}
 
 case class InterfaceDefinition(interfaceName: String, parents: List[RefType],
-    modifiers: List[Modifier], methods: List[MethodDeclaration]) extends TypeDefinition(interfaceName)
+    modifiers: List[Modifier], methods: List[MethodDeclaration]) extends TypeDefinition(interfaceName) {
+    def display: Unit = {
+        println("*" * 20)
+        println("Interface declaration")
+        println("*" * 20)
+        println(s"Interface name: $interfaceName")
+        println(s"Number of parents: ${parents.size}")
+        for (RefType(x) <- parents) println(s"Interface extending: ${x.toString}")
+        for (x <- modifiers) println(s"Modifier: ${x.toString()}")
+        println(s"Number of methods: ${methods.size}")
+        println("*" * 20)
+        println()
+        for (meth <- methods) meth.display
+    }
+}
 
-case class ClassDefinition(className: String, parent: Some[RefType], interfaces: List[RefType], modifiers: List[Modifier],
-    fields: List[FieldDeclaration], methods: List[MethodDeclaration],
-    constructors: List[ConstructorDeclaration]) extends TypeDefinition(className)
+case class ClassDefinition(className: String, parent: Option[RefType], interfaces: List[RefType], modifiers: List[Modifier], fields: List[FieldDeclaration], constructors: List[ConstructorDeclaration], methods: List[MethodDeclaration]) extends TypeDefinition(className) {
+    def display: Unit = {
+        println("*" * 20)
+        println("Class declaration")
+        println("*" * 20)
+        println(s"Class name: $className")
+        println(s"Has parent: ${parent.isDefined}")
+        for (RefType(x) <- parent) println(s"Class extending: ${x.toString}")
+        println(s"Number of implemented interfaces: ${interfaces.size}")
+        for (RefType(x) <- interfaces) println(s"Interface implemented: ${x.toString}")
+        for (x <- modifiers) println(s"Modifier: ${x.toString()}")
+        println(s"Number of fields: ${fields.size}")
+        println(s"Number of constructors: ${constructors.size}") 
+        println(s"Number of methods: ${methods.size}")
+        println("*" * 20)
+        println()
+        for (field <- fields) field.display
+        for (constructor <- constructors) constructor.display
+        for (method <- methods) method.display
+    }
+}
 
 //What can be put in a class
 case class MethodDeclaration(methodName: String, returnType: Type, modifiers: List[Modifier],
-    parameters: List[(Type, String)], implementation: Some[Block])
+    parameters: List[(Type, String)], implementation: Some[Block]) {
+    def display: Unit = {
+        //TODO something fancy...
+    }
+}
+
 case class FieldDeclaration(fieldName: String, fieldType: Type, modifiers: List[Modifier],
-    initializer: Option[Expression])
-case class ConstructorDeclaration(modifiers: List[Modifier], parameters: List[(Type, String)], implementation: Block)
+    initializer: Option[Expression]) {
+    def display: Unit = {
+        //TODO something fancy...
+    }
+}
+case class ConstructorDeclaration(modifiers: List[Modifier], parameters: List[(Type, String)], implementation: Block) {
+    def display: Unit = {
+        //TODO something fancy...
+    }
+}
 
         
 
