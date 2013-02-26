f328c9d34fa359db5ac8013bdf3c800a6ea94598
bastienjacot-guillarmod <bjacotg@gmail.com>
2013-02-26 00:29:03 -0500

    Small tweak on marmoset test, so we know which test failed

--- test/main/MarmosetTest.scala
+++ test/main/MarmosetTest.scala
@@ -5,18 +5,20 @@ import java.io.File
 import scala.io.Source
 
 class MarmosetTest extends FunSuite{
-    test("Test all the java"){
-        val marmDir = new File("test/javaCode/a1marmoset")
-        val success = marmDir.listFiles.sortBy(_.getName()).filter( x =>
-            if(x.getName.startsWith("Je")==(42==Joosc.check(Source.fromFile(x), x.getPath))){
-                println(s"File: ${x.getName} PASSED")
-                true
-            } else{
-                println(s"File: ${x.getName} FAILED")
-                false
-            }
-        ).size
-        
-        println(s"Success rate : $success/320 = ${100*success/320}%")
-    }
+  test("Test all the java"){
+    val marmDir = new File("test/javaCode/a1marmoset")
+    val failure = marmDir.listFiles.sortBy(_.getName()).filter( x =>
+      if(x.getName.startsWith("Je")==(42==Joosc.check(Source.fromFile(x), x.getPath))){
+        println(s"File: ${x.getName} PASSED")
+        false
+      } else{
+        println(s"File: ${x.getName} FAILED")
+        true
+      }
+    )
+    
+    println(s"Success rate : ${320- failure.size}/320 = ${100-100*failure.size/320}%")
+    println("Failed test:")
+    failure.foreach(x => println(x.getName))
+  }
 }
