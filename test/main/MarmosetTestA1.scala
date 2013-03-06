c69f40c68b9b2f7d1ca3f58df8c6373fda91e982
aero-z <aero_super@yahoo.com>
2013-03-05 23:54:38 -0500

    misc git stuff and marmoset test

--- test/main/MarmosetTestA1.scala
+++ test/main/MarmosetTestA1.scala
@@ -4,21 +4,14 @@ import org.scalatest.FunSuite
 import java.io.File
 import scala.io.Source
 
-class MarmosetTestA1 extends FunSuite{
-  test("Test all the java"){
-    val marmDir = new File("test/javaCode/a1marmoset")
-    val failure = marmDir.listFiles.sortBy(_.getName()).filter( x =>
-      if(x.getName.startsWith("Je")==(42==Joosc.check((Source.fromFile(x), x.getName) :: StdlibFiles.stdlibFiles))){
-        println(s"File: ${x.getName} PASSED")
-        false
-      } else{
-        println(s"File: ${x.getName} FAILED")
-        true
-      }
-    )
-    
-    println(s"Success rate : ${320- failure.size}/320 = ${100-100*failure.size/320}%")
-    println("Failed test:")
-    failure.foreach(x => println(x.getName))
-  }
+class MarmosetTestA1 extends FunSuite {
+  val marmDir = new File("test/javaCode/a1marmoset")
+  marmDir.listFiles.sortBy(_.getName()).foreach(file => {
+    val errCode =
+      if (file.getName.startsWith("Je")) Joosc.errCodeParseErr
+      else Joosc.errCodeSuccess
+    test(file.getName) {
+      assert(Joosc.check((Source.fromFile(file), file.getPath) :: StdlibFiles.stdlibFiles) === errCode)
+    }
+  })
 }
--- test/main/MarmosetTestA2.scala
+++ test/main/MarmosetTestA2.scala
@@ -4,28 +4,30 @@ import org.scalatest.FunSuite
 import java.io.File
 import scala.io.Source
 
-class MarmosetTestA2 extends FunSuite {  
-  {
-    def getFiles(file: File, depth: Int): Seq[File] = {
-      if (file.isDirectory() && depth != 0)
-        file.listFiles().sortBy(_.getName()).flatMap(getFiles(_, depth-1))
-      else
-        file :: Nil
-    }
-    
-    val marmDir = new File("test/javaCode/a2marmoset")
-    val testCaseFiles = getFiles(marmDir, 1)
-    testCaseFiles.foreach(file => {
-      val testSources = getFiles(file, -1).map(f => (Source.fromFile(f), f.getPath))
-      /*println("Files:")
-      testSources.foreach(x=> println("- " + x._2))
-      println("- stdlib files")*/
-      val errCode =
-        if (file.getName.startsWith("Je")) Joosc.errCodeParseErr
-        else                               Joosc.errCodeSuccess
-      test(file.getName) {
-        assert(Joosc.check(testSources ++: StdlibFiles.stdlibFiles) === errCode)
-      }
-    })
+class MarmosetTestA2 extends FunSuite {
+
+  def getFiles(file: File, depth: Int): Seq[File] = {
+    if (file.isDirectory() && depth != 0)
+      file.listFiles().sortBy(_.getName()).flatMap(getFiles(_, depth - 1))
+    else
+      file :: Nil
   }
+
+  val marmDir = new File("test/javaCode/a2marmoset")
+  val testCaseFiles = getFiles(marmDir, 1)
+  testCaseFiles.foreach(file => {
+    val testSources = getFiles(file, -1).map(f => (Source.fromFile(f), f.getPath))
+    /*
+    println("Files:")
+    testSources.foreach(x=> println("- " + x._2))
+    println("- stdlib files")
+    */
+    val errCode =
+      if (file.getName.startsWith("Je")) Joosc.errCodeParseErr
+      else                               Joosc.errCodeSuccess
+    test(file.getName) {
+      assert(Joosc.check(testSources ++: StdlibFiles.stdlibFiles) === errCode)
+    }
+  })
+
 }
