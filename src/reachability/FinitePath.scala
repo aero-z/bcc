927a4230548dc807721157f5431899142b29effe
CedricBastin <cedricbastin@gmail.com>
2013-03-18 17:54:28 -0400

    created package for reachability

---
+++ src/reachability/FinitePath.scala
@@ -0,0 +1,11 @@
+package reachability
+
+import ast._
+
+class ReturnException(message:String) extends CompilerError(message, "Return check ")
+
+class FinitePath {
+	def check(meth:MethodDeclaration) {
+	  
+	}
+}
