03f6f20c4366563a7e76b85a2d07d12cd9140f6f
bastienjacot-guillarmod <bjacotg@gmail.com>
2013-04-02 20:30:27 -0400

    Should work

--- test/main/MarmosetTestA5.scala
+++ test/main/MarmosetTestA5.scala
@@ -30,30 +30,70 @@ class MarmosetTestA5 extends FunSuite {
     
     test(file.getName) {
       //TODO generate the code...
-      val writer = new BufferedWriter(new FileWriter(new File(outputDir.getPath + "/rouge.s")))
-      writer.write("""
-global _start
-_start:
 
-mov eax, 1
-mov ebx, 0
-int 0x80
-""")
-      writer.close
+
+
+
+   //    val writer = new BufferedWriter(new FileWriter(new File(outputDir.getPath + "/rouge.s")))
+//       writer.write("""
+//     ; Hello World in assembly for mac
+//     ; nasm -f macho hello.asm
+//     ; ld -e _start -o hello hello.o
+//     ;
+//     section .text
+//     global _start ;must be declared for linker (ld)
+     
+//     _syscall:
+//     int 0x80 ;system call
+//     ret
+     
+//     _start: ;tell linker entry point
+     
+//     push dword len ;message length
+//     push dword msg ;message to write
+//     push dword 1 ;file descriptor (stdout)
+//     mov eax,0x4 ;system call number (sys_write)
+//     call _syscall ;call kernel
+     
+//     ;the alternate way to call kernel:
+//     ;push eax
+//     ;call 7:0
+     
+//     add esp,12 ;clean stack (3 arguments * 4)
+     
+//     push dword 0 ;exit code
+//     mov eax,0x1 ;system call number (sys_exit)
+//     call _syscall ;call kernel
+     
+//     ;we do not return from sys_exit,
+//     ;there's no need to clean stack
+//     section .data
+     
+//     msg db "Hello, world!",0xa ;our dear string
+//     len equ $ - msg ;length of our dear string
+// """)
+//       writer.close
       //assembling
-      outputDir.listFiles.foreach(asm => Seq("nasm", "-O1", "-f", "elf" ,"-g", "-F", "dwarf", asm.getPath).run)
       //Linking, but warning my mac at least does not have the same linker as marmoset
       //outputDir.listFiles.foreach(file => println(file.getPath))
-      if(System.getProperties.get("os.name")== "Mac OS X"){
-        (Seq("ld","-macosx_version_min","10.6", "-arch", "i386", "-o", outputDir.getPath + "/main") ++ outputDir.listFiles.map(_.getPath).filter(_.endsWith(".o"))).run
-      }else{
-        Seq("ld", "-melf_i386", "-o", outputDir.getPath + "/main", outputDir.getPath + "/*.o").run
-      }
+      try{
+        if(System.getProperties.get("os.name")== "Mac OS X"){
+          outputDir.listFiles.foreach(asm => Seq("nasm", "-f", "macho", asm.getPath).run)
+            (Seq("ld", "-macosx_version_min", "10.6", "-e", "_start", "-o", outputDir.getPath + "/main") ++ outputDir.listFiles.map(_.getPath).filter(_.endsWith(".o"))).run
+        }else{
+          outputDir.listFiles.foreach(asm => Seq("nasm", "-O1", "-f", "elf" ,"-g", "-F", "dwarf", asm.getPath).run)
+          Seq("ld", "-melf_i386", "-o", outputDir.getPath + "/main", outputDir.getPath + "/*.o").run
+        }
 
+      
+        Seq(outputDir.getPath + "/main").run
+      }finally{
+        outputDir.listFiles.foreach(src => src.delete)
+      }
       //It was not written any where but I suppose that all the code return 123...
   //    assert(Seq(outputDir.getPath + "/main").! == 123)
       
-      outputDir.listFiles.foreach(src => src.delete)
+
 
     }
   })
