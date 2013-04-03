81ba8821604e01f8035814b085a878401bbaf27d
aero-z <aero_super@yahoo.com>
2013-04-02 21:46:00 -0400

    Added some delays to compilation commands in marmoset test 5

--- test/main/MarmosetTestA5.scala
+++ test/main/MarmosetTestA5.scala
@@ -16,7 +16,7 @@ class MarmosetTestA5 extends FunSuite {
   //Check if output dir is created and clean it...
   val outputDir = new File("test/javaCode/a5marmoset/output")
   outputDir.mkdir
-  outputDir.listFiles.foreach(src => src.delete) 
+  outputDir.listFiles.foreach(src => src.delete)
 
   val marmDir = new File("test/javaCode/a5marmoset")
   val testCaseFiles = getFiles(marmDir, 1).filter(_.getName != "output")
@@ -27,73 +27,74 @@ class MarmosetTestA5 extends FunSuite {
     testSources.foreach(x=> println("- " + x._2))
     println("- stdlib files")
     */
-    
+
     test(file.getName) {
       //TODO generate the code...
 
-
-
-
-   //    val writer = new BufferedWriter(new FileWriter(new File(outputDir.getPath + "/rouge.s")))
-//       writer.write("""
-//     ; Hello World in assembly for mac
-//     ; nasm -f macho hello.asm
-//     ; ld -e _start -o hello hello.o
-//     ;
-//     section .text
-//     global _start ;must be declared for linker (ld)
-     
-//     _syscall:
-//     int 0x80 ;system call
-//     ret
-     
-//     _start: ;tell linker entry point
-     
-//     push dword len ;message length
-//     push dword msg ;message to write
-//     push dword 1 ;file descriptor (stdout)
-//     mov eax,0x4 ;system call number (sys_write)
-//     call _syscall ;call kernel
-     
-//     ;the alternate way to call kernel:
-//     ;push eax
-//     ;call 7:0
-     
-//     add esp,12 ;clean stack (3 arguments * 4)
-     
-//     push dword 0 ;exit code
-//     mov eax,0x1 ;system call number (sys_exit)
-//     call _syscall ;call kernel
-     
-//     ;we do not return from sys_exit,
-//     ;there's no need to clean stack
-//     section .data
-     
-//     msg db "Hello, world!",0xa ;our dear string
-//     len equ $ - msg ;length of our dear string
-// """)
-//       writer.close
+      /* // DUMMY CODE
+      val writer = new BufferedWriter(new FileWriter(new File(outputDir.getPath + "/rouge.s")))
+      writer.write("""
+          
+ ; Hello World in assembly for mac
+ ; nasm -f macho hello.asm
+ ; ld -e _start -o hello hello.o
+ ;
+ section .text
+ global _start ;must be declared for linker (ld)
+ 
+ _syscall:
+ int 0x80 ;system call
+ ret
+ 
+ _start: ;tell linker entry point
+ 
+ push dword len ;message length
+ push dword msg ;message to write
+ push dword 1 ;file descriptor (stdout)
+ mov eax,0x4 ;system call number (sys_write)
+ call _syscall ;call kernel
+ 
+ ;the alternate way to call kernel:
+ ;push eax
+ ;call 7:0
+ 
+ add esp,12 ;clean stack (3 arguments * 4)
+ 
+ push dword 0 ;exit code
+ mov eax,0x1 ;system call number (sys_exit)
+ call _syscall ;call kernel
+ 
+ ;we do not return from sys_exit,
+ ;there's no need to clean stack
+ section .data
+ 
+ msg db "Hello, world!",0xa ;our dear string
+ len equ $ - msg ;length of our dear string
+          
+ """)
+      writer.close
+      */
+      
       //assembling
       //Linking, but warning my mac at least does not have the same linker as marmoset
       //outputDir.listFiles.foreach(file => println(file.getPath))
-      try{
-        if(System.getProperties.get("os.name")== "Mac OS X"){
+      try {
+        if (System.getProperties.get("os.name") == "Mac OS X") {
           outputDir.listFiles.foreach(asm => Seq("nasm", "-f", "macho", asm.getPath).run)
-            (Seq("ld", "-macosx_version_min", "10.6", "-e", "_start", "-o", outputDir.getPath + "/main") ++ outputDir.listFiles.map(_.getPath).filter(_.endsWith(".o"))).run
-        }else{
-          outputDir.listFiles.foreach(asm => Seq("nasm", "-O1", "-f", "elf" ,"-g", "-F", "dwarf", asm.getPath).run)
-          Seq("ld", "-melf_i386", "-o", outputDir.getPath + "/main", outputDir.getPath + "/*.o").run
+          (Seq("ld", "-macosx_version_min", "10.6", "-e", "_start", "-o", outputDir.getPath + "/main") ++ outputDir.listFiles.map(_.getPath).filter(_.endsWith(".o"))).run
+        } else {
+          outputDir.listFiles.foreach(asm => Seq("nasm", "-O1", "-f", "elf", "-g", "-F", "dwarf", asm.getPath).run)
+          Thread.sleep(200);
+          Seq("ld", "-melf_i386", "-o", outputDir.getPath + "/main", outputDir.getPath + "/rouge.o").run
+          Thread.sleep(200);
         }
 
-      
         Seq(outputDir.getPath + "/main").run
-      }finally{
+      } finally {
         outputDir.listFiles.foreach(src => src.delete)
       }
-      //It was not written any where but I suppose that all the code return 123...
-  //    assert(Seq(outputDir.getPath + "/main").! == 123)
-      
-
+      //It was not written anywhere but I suppose that all the code return 123...
+      //    assert(Seq(outputDir.getPath + "/main").! == 123)
 
     }
   })
